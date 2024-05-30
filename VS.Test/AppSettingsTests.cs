ecdff229d53702891f39d403534a1f717b07d142
Fábio Dias <fabiodcs@gmail.com>
2024-05-30 18:40:23 -0300

    Add project files.

---
+++ VS.Test/AppSettingsTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace Core.Service.Test.AppSettings;
+
+public class AppSettingsTests
+{
+    [Fact]
+    public void ShouldHaveTestSetting()
+    {
+        // Arrange
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        var configuration = builder.Build();
+
+        string appsettingsContent = File.ReadAllText($"{AppContext.BaseDirectory}/appsettings.json");
+
+        // Act
+        string testSetting = configuration.GetSection("TestSetting").Value;
+
+        // Assert
+        testSetting.Should().NotBeNull(appsettingsContent);
+    }
+}
